8408741208ccd57bdbc8eeab98a8144e16b5a0c3
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-28 23:38:34 +0300

    Initial commit

---
+++ Swiftbook/Swiftbook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiftbook
+//
+//  Created by yauheni prakapenka on 28.01.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
