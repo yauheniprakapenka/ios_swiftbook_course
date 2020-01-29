795cf6f1e638ad62b7ae23165b30e4495e043ae5
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-29 18:29:42 +0300

    [Lesson1][Task2] Initial commit

---
+++ Swiftbook/Lesson1/Task2/TrafficTight/TrafficTight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficTight
+//
+//  Created by yauheni prakapenka on 29.01.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
