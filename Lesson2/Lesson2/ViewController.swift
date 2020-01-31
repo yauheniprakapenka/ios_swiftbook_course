6265e03fbc476d276fec598e2f606fec86a3a885
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-31 18:45:57 +0300

    Created UI, configed sliders

--- Lesson2/Lesson2/ViewController.swift
+++ Lesson2/Lesson2/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redTextField: UITextField!
+    @IBOutlet var greenTextField: UITextField!
+    @IBOutlet var blueTextField: UITextField!
+    
+    @IBOutlet var changeableColorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configSliders()
     }
-
-
+    
+    private func configSliders() {
+        redSlider.minimumValue              = 0
+        redSlider.maximumValue              = 1
+        redSlider.value                     = 1
+        redSlider.minimumTrackTintColor     = .red
+        
+        greenSlider.minimumValue            = 0
+        greenSlider.maximumValue            = 1
+        greenSlider.value                   = 1
+        greenSlider.minimumTrackTintColor   = .green
+        
+        blueSlider.minimumValue             = 0
+        blueSlider.maximumValue             = 1
+        blueSlider.value                    = 1
+        blueSlider.minimumTrackTintColor    = .blue
+    }
 }
-
