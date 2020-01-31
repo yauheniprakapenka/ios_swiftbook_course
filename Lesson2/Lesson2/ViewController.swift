ecd72acc3de9863d0d1fb7f839e5a4f4a3592017
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-31 19:57:56 +0300

    Did 1 task of lesson 2

--- Lesson2/Lesson2/ViewController.swift
+++ Lesson2/Lesson2/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    enum SliderId: String {
+        case redSliderID, greenSliderID, blueSliderID
+    }
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
@@ -27,22 +31,77 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configSliders()
+        configLabels()
+        congigTextFields()
+        
+        changeableColorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                                      green: CGFloat(greenSlider.value),
+                                                      blue: CGFloat(redSlider.value),
+                                                      alpha: 1)
+    }
+    
+    @IBAction func valueSliderChanged(_ sender: UISlider) {
+        let currentSliderValue = String(format: "%.2f", sender.value)
+        
+        var redOpacity      = CGFloat(redSlider.value)
+        var greenOpacity    = CGFloat(greenSlider.value)
+        var blueOpacity     = CGFloat(blueSlider.value)
+        
+        switch sender.accessibilityIdentifier {
+        case SliderId.redSliderID.rawValue:
+            redLabel.text           = currentSliderValue
+            redTextField.text       = currentSliderValue
+            redOpacity              = CGFloat(sender.value)
+            
+        case SliderId.greenSliderID.rawValue:
+            greenLabel.text         = currentSliderValue
+            greenTextField.text     = currentSliderValue
+            greenOpacity            = CGFloat(sender.value)
+            
+        case SliderId.blueSliderID.rawValue:
+            blueLabel.text          = currentSliderValue
+            blueTextField.text      = currentSliderValue
+            blueOpacity             = CGFloat(sender.value)
+            
+        default:
+            print("Error getting value from slider")
+        }
+        
+        changeableColorView.backgroundColor = UIColor(red: redOpacity,
+                                                      green: greenOpacity,
+                                                      blue: blueOpacity,
+                                                      alpha: 1)
     }
     
     private func configSliders() {
         redSlider.minimumValue              = 0
         redSlider.maximumValue              = 1
-        redSlider.value                     = 1
+        redSlider.value                     = 0.5
         redSlider.minimumTrackTintColor     = .red
+        redSlider.accessibilityIdentifier   = SliderId.redSliderID.rawValue
         
         greenSlider.minimumValue            = 0
         greenSlider.maximumValue            = 1
-        greenSlider.value                   = 1
+        greenSlider.value                   = 0.5
         greenSlider.minimumTrackTintColor   = .green
+        greenSlider.accessibilityIdentifier = SliderId.greenSliderID.rawValue
         
         blueSlider.minimumValue             = 0
         blueSlider.maximumValue             = 1
-        blueSlider.value                    = 1
+        blueSlider.value                    = 0.5
         blueSlider.minimumTrackTintColor    = .blue
+        blueSlider.accessibilityIdentifier  = SliderId.blueSliderID.rawValue
+    }
+    
+    private func configLabels() {
+        redLabel.text       = String(format: "%.2f", redSlider.value)
+        greenLabel.text     = String(format: "%.2f", greenSlider.value)
+        blueLabel.text      = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func congigTextFields() {
+        redTextField.text   = String(format: "%.2f", redSlider.value)
+        greenTextField.text = String(format: "%.2f", greenSlider.value)
+        blueTextField.text  = String(format: "%.2f", blueSlider.value)
     }
 }
