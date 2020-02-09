19160b16f66a0da582c60ce3b2dd2de1b45b8946
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-02-09 14:14:44 +0300

    [HW2.4] Refactoring

--- HomeWork2.4/ChipAndDale/Controllers/LoginViewController.swift
+++ HomeWork2.4/ChipAndDale/Controllers/LoginViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import QuartzCore
 
-class ViewController: UIViewController, CAAnimationDelegate {
+class LoginViewController: UIViewController, CAAnimationDelegate {
     
     // MARK: - IBOutlets
     
@@ -33,26 +33,9 @@ class ViewController: UIViewController, CAAnimationDelegate {
         super.viewDidAppear(animated)
         
         animateClouds()
-        makeAnimateSun()
-        
-        sunImageViewHeightConstraint.constant = -85
-        makeAnimateLayout(duration: 3.3, delay: 0)
-        
-        headerLabelCenterConstraint.constant = 0
-        makeAnimateLayout(duration: 0.7, delay: 3)
-        
-        usernameTextFieldCenterConstraint.constant = 0
-        makeAnimateLayout(duration: 0.7, delay: 3.4)
-        
-        passwordTextFieldCenterConstraint.constant = 0
-        makeAnimateLayout(duration: 0.7, delay: 3.8)
-        
-        UIView.animate(withDuration: 3, delay: 1, options: .curveEaseInOut, animations: { [weak self] in
-            self?.backgroundView.backgroundColor = #colorLiteral(red: 0.6485911608, green: 0.790573597, blue: 0.9224731922, alpha: 1)
-        })
-        
-        loginButtonHeightConstraint.constant = 40
-        makeAnimateLayout(duration: 1.6, delay: 3.3)
+        animateSun()
+        animateBackground()
+        animateLayouts()
     }
     
     override func viewDidLoad() {
@@ -74,19 +57,35 @@ class ViewController: UIViewController, CAAnimationDelegate {
     // MARK: - Private Methods
     
     private func animateClouds() {
-        makeCloudAnimation(view: cloud1ImageView, duration: 2.8, value: 1.16)
-        makeCloudAnimation(view: cloud2ImageView, duration: 3.0, value: 1.28)
-        makeCloudAnimation(view: cloud3ImageView, duration: 2.4, value: 1.15)
-        makeCloudAnimation(view: cloud4ImageView, duration: 3.2, value: 1.23)
+        animateCloud(view: cloud1ImageView, duration: 2.8, value: 1.16)
+        animateCloud(view: cloud2ImageView, duration: 3.0, value: 1.28)
+        animateCloud(view: cloud3ImageView, duration: 2.4, value: 1.15)
+        animateCloud(view: cloud4ImageView, duration: 3.2, value: 1.23)
+    }
+    
+    private func animateLayouts() {
+        sunImageViewHeightConstraint.constant = -85
+        animateLayout(duration: 3.3, delay: 0)
+        
+        headerLabelCenterConstraint.constant = 0
+        animateLayout(duration: 0.9, delay: 3)
+        
+        usernameTextFieldCenterConstraint.constant = 0
+        animateLayout(duration: 0.9, delay: 3.4)
+        
+        passwordTextFieldCenterConstraint.constant = 0
+        animateLayout(duration: 0.9, delay: 3.8)
+        
+        loginButtonHeightConstraint.constant = 40
+        animateLayout(duration: 1.9, delay: 4.0)
     }
 }
 
-// MARK: - Cloud Animation
+// MARK: - Animation
 
-private extension ViewController {
+private extension LoginViewController {
     
-    func makeCloudAnimation(view: UIView, duration: Double, value: CGFloat) {
-        
+    func animateCloud(view: UIView, duration: Double, value: CGFloat) {
         let options: UIView.AnimationOptions = [.curveEaseInOut, .repeat, .autoreverse]
         
         UIView.animate(withDuration: duration, delay: 0, options: options, animations: {
@@ -94,13 +93,8 @@ private extension ViewController {
             view.frame.size.width *= value
         }, completion: nil)
     }
-}
-
-// MARK: - Sun Animation
-
-private extension ViewController {
     
-    func makeAnimateSun() {
+    func animateSun() {
         let fullRotation = CABasicAnimation(keyPath: "transform.rotation")
         fullRotation.delegate = self
         fullRotation.fromValue = NSNumber(floatLiteral: 0)
@@ -109,16 +103,16 @@ private extension ViewController {
         fullRotation.repeatCount = .infinity
         sunImageView.layer.add(fullRotation, forKey: "360")
     }
-}
-
-// MARK: - Layout Animation
-
-private extension ViewController {
     
-    func makeAnimateLayout(duration: Double, delay: Double) {
+    func animateLayout(duration: Double, delay: Double) {
         UIView.animate(withDuration: duration, delay: delay, options: [.curveEaseInOut], animations: { [weak self] in
             self?.view.layoutIfNeeded()
         })
     }
+    
+    func animateBackground() {
+        UIView.animate(withDuration: 3, delay: 1, options: .curveEaseInOut, animations: { [weak self] in
+            self?.backgroundView.backgroundColor = #colorLiteral(red: 0.6485911608, green: 0.790573597, blue: 0.9224731922, alpha: 1)
+        })
+    }
 }
-
---
+++ HomeWork2.4/ChipAndDale/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  ChipAndDale
+//
+//  Created by yauheni prakapenka on 09.02.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+struct User {
+    var name: String
+    var password: String
+    
+    static let Users = [User(name: "chip", password: "dale")]
+}
